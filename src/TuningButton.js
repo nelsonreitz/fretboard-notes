569832b6820e80547cefafa0507daf044a4c4812
Nelson Reitz <nailsonne@gmail.com>
2018-12-21 10:53:18 +0100

    Add TuningButton component file

---
+++ src/TuningButton.js
@@ -0,0 +1,25 @@
+import React, { component } from 'react';
+
+class TuningButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.onTuningSelect(e.target.value);
+  }
+
+  render() {
+    const tuningAbbr = this.props.tuningAbbr;
+    const tuningName = this.props.tuningName;
+
+    return (
+      <button className="TuningBtn" value={tuningAbbr} onClick={this.handleClick}>
+        {tuningName}
+      </button>
+    );
+  }
+}
+
+export default TuningButton;
--- src/TuningSelect.js
+++ src/TuningSelect.js
@@ -1,26 +1,5 @@
 import React, { Component } from 'react';
-
-class TuningButton extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    this.props.onTuningSelect(e.target.value);
-  }
-
-  render() {
-    const tuningAbbr = this.props.tuningAbbr;
-    const tuningName = this.props.tuningName;
-
-    return (
-      <button className="TuningBtn" value={tuningAbbr} onClick={this.handleClick}>
-        {tuningName}
-      </button>
-    );
-  }
-}
+import TuningButton from './TuningButton.js';
 
 class TuningSelect extends Component {
   render() {
