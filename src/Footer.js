42d00ecb059196d100778fcd87c3d5aada9b7ed1
Nelson Reitz <nailsonne@gmail.com>
2018-12-21 17:58:01 +0100

    Add Footer style

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,9 @@ class Footer extends Component {
     return (
       <footer className="Footer">
         <div className="Container">
-          2018
-          <a href="http://nelsonreitz.ch" target="_blank" rel="noopener noreferrer">Nelson Reitz</a>
+          <div className="Copyright">
+            2018  &copy; <a href="http://nelsonreitz.ch" target="_blank" rel="noopener noreferrer">Nelson Reitz</a>
+          </div>
           <a href="http://github.com/nelsonreitz/fretboard" target="_blank" rel="noopener noreferrer">View the source on Github</a>
         </div>
       </footer>
