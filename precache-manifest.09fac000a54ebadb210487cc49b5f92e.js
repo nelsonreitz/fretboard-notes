d005a567729e4da42643fbc5fbe019e2c02717d1
Nelson Reitz <nailsonne@gmail.com>
2018-12-23 16:57:24 +0100

    Updates

---
+++ precache-manifest.09fac000a54ebadb210487cc49b5f92e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e008833ea60f99443aa7",
+    "url": "/fretboard-notes/static/js/runtime~main.e008833e.js"
+  },
+  {
+    "revision": "773b73199e0efbe2aac4",
+    "url": "/fretboard-notes/static/js/main.773b7319.chunk.js"
+  },
+  {
+    "revision": "ee041ca33cead9a9a7e8",
+    "url": "/fretboard-notes/static/js/1.ee041ca3.chunk.js"
+  },
+  {
+    "revision": "773b73199e0efbe2aac4",
+    "url": "/fretboard-notes/static/css/main.bcb5bcc5.chunk.css"
+  },
+  {
+    "revision": "70a2067419fe4f06cbce4af436ccc529",
+    "url": "/fretboard-notes/index.html"
+  }
+];
