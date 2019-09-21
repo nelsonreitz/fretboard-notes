51763ee97f0da8de55e7a92cc4ec3df52ebf9026
Nelson Reitz <nelson@antistatique.net>
2019-09-21 16:39:35 +0200

    refactor(footer): change to function component

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="Footer">
-        <div className="Container">
-          <div className="Copyright">
-            {new Date().getFullYear()} &copy; <a href="http://nelsonreitz.ch" target="_blank" rel="noopener noreferrer">Nelson Reitz</a>
-          </div>
-          <a href="http://github.com/nelsonreitz/fretboard" target="_blank" rel="noopener noreferrer">View the source on Github</a>
+function Footer() {
+  return (
+    <footer className="Footer">
+      <div className="Container">
+        <div className="Copyright">
+          {new Date().getFullYear()} &copy; <a href="http://nelsonreitz.ch" target="_blank" rel="noopener noreferrer">Nelson Reitz</a>
         </div>
-      </footer>
-    );
-  }
+        <a href="http://github.com/nelsonreitz/fretboard" target="_blank" rel="noopener noreferrer">View the source on Github</a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
