0ed1c643a621d07d714b7722a89cb425c3619a44
Nelson Reitz <nelson@antistatique.net>
2019-09-21 17:08:21 +0200

    refactor(tuningSelect): change to function component

--- src/TuningSelect.js
+++ src/TuningSelect.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TuningButton from './TuningButton.js';
 import './TuningSelect.css';
 
-class TuningSelect extends Component {
-  render() {
-   const buttons = [];
+function TuningSelect({tunings, activeTuning, onTuningSelect}) {
 
-    // Create buttons
-    for (let tuning of this.props.tunings) {
-      buttons.push(
-        <TuningButton
-          key={tuning.abbr}
-          tuningAbbr={tuning.abbr}
-          tuningName={tuning.name}
-          isActive={tuning.abbr === this.props.activeTuning}
-          onTuningSelect={this.props.onTuningSelect}
-        />
-      );
-    }
-
-    return (
-      <div className="TuningSelect">
-        <div className="Container">
-          <fieldset>
-            <legend>Choose a tuning:</legend>
-            {buttons}
-          </fieldset>
-        </div>
-      </div>
+  // Create buttons
+  const buttons = [];
+  for (let tuning of tunings) {
+    buttons.push(
+      <TuningButton
+        key={tuning.abbr}
+        tuningAbbr={tuning.abbr}
+        tuningName={tuning.name}
+        isActive={tuning.abbr === activeTuning}
+        onTuningSelect={onTuningSelect}
+      />
     );
   }
+
+  return (
+    <div className="TuningSelect">
+      <div className="Container">
+        <fieldset>
+          <legend>Choose a tuning:</legend>
+          {buttons}
+        </fieldset>
+      </div>
+    </div>
+  );
 }
 
 export default TuningSelect;
