64ed04fc644239276a60373c7cac83a5c8fea593
Nelson Reitz <nelson@antistatique.net>
2019-09-21 16:41:36 +0200

    refactor(header): change to function component

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="Header">
-        <div className="Container">
-          <h1 className="Sitetitle">Fretboard Notes</h1>
-          <p className="SiteDescription">Visual chart to learn guitar notes</p>
-        </div>
-      </header>
-    );
-  }
+function Header() {
+  return (
+    <header className="Header">
+      <div className="Container">
+        <h1 className="Sitetitle">Fretboard Notes</h1>
+        <p className="SiteDescription">Visual chart to learn guitar notes</p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
